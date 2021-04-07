7365b8b5906dae73e38cb86c8d43998053374fb9
iuliusavgvstvs <iulius.satmari@gmail.com>
2021-04-07 13:47:26 +0300

    created empty project

---
+++ React Native/rn-guess-number/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+const Header = props => {
+  <View>
+    <Text>{props/title}</Text>
+  </View>
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Header;
